43f640a22f8e82a2a9ae5d31fea42fac7f34df28
Wojciech Turek <wojciechturek@outlook.com>
2020-07-07 18:59:13 +0200

    Updates

---
+++ precache-manifest.35a9ab783f89f0b1b27b8a0e79ce3e2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6929ab3ffa392500d34f01faabd0105",
+    "url": "/bestbann/index.html"
+  },
+  {
+    "revision": "3968e1194385170d8501",
+    "url": "/bestbann/static/css/main.0e2525e3.chunk.css"
+  },
+  {
+    "revision": "96e269626a1024521462",
+    "url": "/bestbann/static/js/2.d9d49c9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bestbann/static/js/2.d9d49c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3968e1194385170d8501",
+    "url": "/bestbann/static/js/main.af0c37af.chunk.js"
+  },
+  {
+    "revision": "e5e81b53cee179319d23",
+    "url": "/bestbann/static/js/runtime-main.224ef31d.js"
+  },
+  {
+    "revision": "417aab92a1ce51b52febbd38cb099c0b",
+    "url": "/bestbann/static/media/herobanner.417aab92.jpg"
+  }
+]);
