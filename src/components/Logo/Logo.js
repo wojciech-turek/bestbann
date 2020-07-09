93f8055e417b51b00003421066afd300ce19875b
Wojciech Turek <wojciechturek@outlook.com>
2020-07-09 21:19:46 +0200

    added locale to menu

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from "./Logo.module.css";
 import bestBannLogo from "../../assets/logo-small.png";
+import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className={classes.Logo}>
-      <img src={bestBannLogo} alt="Best Bann Logo" />
+      <NavLink to="/">
+        <img src={bestBannLogo} alt="Best Bann Logo" />
+      </NavLink>
     </div>
   );
 }
--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./NavigationItems.module.css";
+import { useTranslation } from "react-i18next";
 
 export default function NavigationItems() {
+  const { t } = useTranslation("common");
   return (
     <div className={classes.NavItems}>
-      <NavigationItem link="/">Main Page</NavigationItem>
-      <NavigationItem link="/products">Products</NavigationItem>
-      <NavigationItem link="/about">About Us</NavigationItem>
-      <NavigationItem link="/contact">Contact</NavigationItem>
+      <NavigationItem link="/">{t("menu.mainPage")}</NavigationItem>
+      <NavigationItem link="/products">{t("menu.products")}</NavigationItem>
+      <NavigationItem link="/about">{t("menu.aboutUs")}</NavigationItem>
+      <NavigationItem link="/contact">{t("menu.contact")}</NavigationItem>
     </div>
   );
 }
