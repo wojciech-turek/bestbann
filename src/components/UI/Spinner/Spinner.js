5eadf125901801d9f19780f201cbf7d32c69b8f3
Wojciech Turek <wojciechturek@outlook.com>
2020-08-23 23:07:17 +0200

    changed loading animation

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -6,20 +6,12 @@ import classes from "./Spinner.module.css";
 function Spinner(props) {
   let app = useRef(null);
   let circle = useRef(null);
-  let circleRed = useRef(null);
-  let circleBlue = useRef(null);
 
   useEffect(() => {
     TweenMax.to(app, 0, { css: { visibility: "visible" } });
-    TweenMax.staggerFrom(
-      [circle, circleRed, circleBlue],
-      1.8,
-      { opacity: 0, x: 80, ease: Power3.easeOut },
-      0.2
-    );
-
     setTimeout(() => {
-      TweenMax.to(app, 0.5, { css: { opacity: 0 } });
+      TweenMax.to(circle, 0, { css: { opacity: 0 } });
+      TweenMax.to(app, 0.2, { css: { opacity: 0 } });
     }, 700);
     setTimeout(() => {
       props.setLoading(false);
@@ -30,9 +22,9 @@ function Spinner(props) {
     <div className={classes.Spinner} ref={(el) => (app = el)}>
       <header className={classes.SpinnerHeader}>
         <div className={classes.circleContainer}>
-          <div ref={(el) => (circle = el)} className={classes.circle}></div>
-          <div ref={(el) => (circleRed = el)} className={classes.circle}></div>
-          <div ref={(el) => (circleBlue = el)} className={classes.circle}></div>
+          <p ref={(el) => (circle = el)} className={classes.circle}>
+            Loading...
+          </p>
         </div>
       </header>
     </div>
