5f1c0af522b46ec7d010955751ce46255e517cde
wojciech-turek <contact@wojciechturek.com>
2024-02-06 11:39:57 +0100

    SEO update

--- src/utils/SEO.js
+++ src/utils/SEO.js
@@ -8,6 +8,20 @@ export default function SEO({ children, location, title, image }) {
   return (
     <Helmet titleTemplate={`Bestbann - %s`}>
       <title>{title}</title>
+      {/* <!-- Google Tag Manager --> */}
+      <script>
+        {`(function (w, d, s, l, i) {
+          w[l] = w[l] || [];
+          w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+          var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : "";
+          j.async = true;
+          j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, "script", "dataLayer", "GTM-NJRJGJ")`}
+      </script>
+      {/* <!-- End Google Tag Manager --> */}
       {children}
     </Helmet>
   );
